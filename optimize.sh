5a88b00c5505f1178ca183fd5e128e60ac06ddec
rieger::CLOUD <riegercloud@users.noreply.github.com>
2018-09-20 21:48:46 +0200

    Optimize your Nextcloud instance
    issue: /usr/local/src/install-nextcloud/optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -2,7 +2,7 @@
 # Optimize your Nextcloud instance
 # create a daily cronjob, e.g.:
 # crontab -e
-# 5 1 * * * /root/optimize.sh > /dev/null 2>&1
+# 5 1 * * *  /usr/local/src/install-nextcloud/optimize.sh 2>&1
 ####
 #!/bin/bash
 redis-cli -s /var/run/redis/redis-server.sock <<EOF
