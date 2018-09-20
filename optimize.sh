c9251fededa3fbf9bfb91d4968114b4d86efb4eb
rieger::CLOUD <riegercloud@users.noreply.github.com>
2018-09-20 21:22:27 +0200

    Optimization for your Nextcloud instance
    Initial script

---
+++ optimize.sh
@@ -0,0 +1,12 @@
+####
+# create a daily cronjob, e.g.
+# 5 1 * * * /scripts/optimize.sh 2&>1
+####
+#!/bin/bash
+redis-cli -s /var/run/redis/redis-server.sock <<EOF
+FLUSHALL
+quit
+EOF
+sudo -u www-data php /var/www/nextcloud/occ files:scan --all
+sudo -u www-data php /var/www/nextcloud/occ files:scan-app-data
+exit 0
