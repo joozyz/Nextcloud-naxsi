4e32a24f4b8101573309ec9b36ed1002a543ceb9
rieger::CLOUD <riegercloud@users.noreply.github.com>
2018-09-20 21:33:30 +0200

    renew your letsencrypt ssl certificates weekly
    letsencrypt renew

---
+++ ssl-renewal.sh
@@ -0,0 +1,23 @@
+####
+# renew your letsencrypt ssl certificates weekly
+# create a cronjob, e.g.:
+# crontab -e
+# @weekly /root/ssl-renewal.sh > /home/username/ssl-renewal.txt
+####
+
+#!/bin/bash
+CURRENT_TIME_FORMATE="%d.%m.%Y"
+cd /etc/letsencrypt
+echo "-------------------------------------"
+echo "Renewal:"
+letsencrypt renew
+result=$(find /etc/letsencrypt/live/ -type l -mtime -1 )
+if [ -n "$result" ]; then
+        /usr/sbin/service nginx stop
+        /usr/sbin/service mysql restart
+        /usr/sbin/service redis-server restart
+        /usr/sbin/service php7.2-fpm restart
+        /usr/sbin/service nginx restart
+fi
+mail -s "Renewal - $(date +$CURRENT_TIME_FORMATE)" -a "FROM: Your Name <your@email.com>" your@email.com < /home/username/ssl-renewal.txt
+exit 0
